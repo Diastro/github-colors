e2157a1526b65c3b8f4d595f7b45b5af9fb59b74
David Albertson <albertson.d@gmail.com>
2015-04-26 12:53:12 -0700

    Adding color to README.md and updating

--- parser.py
+++ parser.py
@@ -13,7 +13,6 @@ def main():
     if not response.ok:
         print "Are you sure the url [%s] is right?" % url
 
-
     languages = yaml.load(response.text)
     output = {}
 
@@ -22,15 +21,20 @@ def main():
             output[key] = value['color']
         except KeyError:
             continue
-	
+
     # json
     open("github-colors.json", "w").write(json.dumps(output, indent=4))
-    
-    #csv
+
+    # csv
     writer = csv.writer(open("github-colors.csv", "w"))
-    writer.writerow(["language","color"])
+    writer.writerow(["language", "color"])
     for key, value in output.items():
         writer.writerow([key, value])
 
+    # colors
+    # insipired by : https://github.com/ozh/github-colors [which isn't being updated]
+    f = open("colors.md", "w")
+    for key, value in output.items():
+        f.write("[![](http://www.placehold.it/150/" + value.replace("#", "") + "/ffffff&text=" + key + ")](https://github.com/trending?l=" + key + ")")
 if __name__ == "__main__":
     main()
