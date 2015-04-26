6733442f5d6e9ab38563217ce2768c06a50c88a9
Myles Braithwaite <me@mylesbraithwaite.com>
2015-04-26 19:50:12 -0400

    Added support for Less and SASS.

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
+import re
 import csv
 import json
+import unicodedata
 
 import yaml
 import requests
@@ -7,6 +9,12 @@ import requests
 
 url = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml'
 
+def slugify(str):
+    # From <http://yashchandra.com/2014/05/08/how-to-generate-clean-url-or-a-slug-in-python/>
+    slug = unicodedata.normalize("NFKD",unicode(str)).encode("ascii", "ignore")
+    slug = re.sub(r"[^\w]+", " ", slug)
+    slug = "-".join(slug.lower().strip().split())
+    return slug
 
 def main():
     response = requests.get(url, stream=True)
@@ -21,20 +29,32 @@ def main():
             output[key] = value['color']
         except KeyError:
             continue
+    
+    # less.js
+    with open("github-colors.less", 'w') as f:
+        for key, value in output.items():
+            f.write('@github-color-%s: %s;\n' % (slugify(key), value.upper()))
+    
+    # sass
+    with open("github-colors.scss", 'w') as f:
+        for key, value in output.items():
+            f.write('$github-color-%s: %s;\n' % (slugify(key), value.upper()))
 
     # json
-    open("github-colors.json", "w").write(json.dumps(output, indent=4))
-
+    with open("github-colors.json", 'w') as f:
+        f.write(json.dumps(output, indent=4))
+    
     # csv
     writer = csv.writer(open("github-colors.csv", "w"))
     writer.writerow(["language", "color"])
     for key, value in output.items():
         writer.writerow([key, value])
-
+    
     # colors
     # insipired by : https://github.com/ozh/github-colors [which isn't being updated]
-    f = open("colors.md", "w")
-    for key, value in output.items():
-        f.write("[![](http://www.placehold.it/150/" + value.replace("#", "") + "/ffffff&text=" + key + ")](https://github.com/trending?l=" + key + ")")
+    with open("colors.md", "w") as f:
+        for key, value in output.items():
+            f.write("[![](http://www.placehold.it/150/" + value.replace("#", "") + "/ffffff&text=" + key + ")](https://github.com/trending?l=" + key + ")")
+
 if __name__ == "__main__":
     main()
